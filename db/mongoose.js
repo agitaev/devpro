0512aa522e21232c8dfbc250f91487d6d06e9de9
Seid Akhmed Agitaev <a.agitaev@gmail.com>
2020-03-19 08:10:22 +0500

    client react and mongodb initial setup

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-	'mongodb://127.0.0.1:27017/devpro_v2',
+	process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/devpro_v2',
 	{
 		useNewUrlParser: true,
 		useFindAndModify: true,
--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const app = express();
 require('./db/mongoose');
 
+const users = require('./routes/userRoutes');
+require('./models/Product');
+
+// BodyParser middleware
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
 app.use(bodyParser.json());
+require('./routes/productRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
+
+// Routes
+app.use('/api/users', users);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
-	console.log(`app running on port ${PORT}`);
+	console.log(`[server] app running on http://127.0.0.1:${PORT}`);
 });
