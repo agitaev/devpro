4db76e9783841c32dffe33f194e69eb7df826f3d
Seid Akhmed Agitaev <a.agitaev@gmail.com>
2020-04-01 15:58:06 +0500

    fix mongodb connection string uri

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
 
-const app = express();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 require('./db/mongoose');
+const app = express();
 
 const users = require('./routes/userRoutes');
 const posts = require('./routes/postRoutes');
@@ -27,8 +28,6 @@ if (process.env.NODE_ENV === 'production') {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
-
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 // Passport middleware
 app.use(passport.initialize());
