52529053f7186d282e49ca854aae1246fef64120
Seid Akhmed Agitaev <a.agitaev@gmail.com>
2020-04-02 12:09:46 +0500

    acquired ssl certificate and https redirect

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const path = require('path');
 const morgan = require('morgan');
+const forceSsl = require('./config/ssl');
 const app = express();
 
 // setup mongodb databse
@@ -53,6 +54,9 @@ app.use('/api/recommender', recommender);
 
 // serve assets in production environment
 if (process.env.NODE_ENV === 'production') {
+	// redirect to https
+	app.use(forceSsl);
+
 	app.use(express.static(path.join(__dirname, 'client/build')));
 	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
@@ -60,6 +64,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => {
 	console.log(`[server] server is running on http://127.0.0.1:${PORT}`);
 });
